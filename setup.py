8967113e6e0ea08af1a7ef4df0efa0006af9288d
Taylor Denouden <taylordenouden@gmail.com>
2019-05-22 17:07:46 -0400

    Add __version__ var to package

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-
-version = '0.1.2'
-
-with open('./README.md') as f:
-    long_description = f.read()
+import mc_uncertainty
 
 setup(
     name='mc-uncertainty',
-    version=version,
+    version=mc_uncertainty.__version__,
     author='Taylor Denouden',
     author_email='taylordenouden@gmail.com',
     packages=find_packages(),
     url='https://github.com/tayden/mc-uncertainty',
     license='MIT',
     description='Calculate uncertainty measures from Monte Carlo sampled model outputs.',
-    long_description=long_description,
+    long_description=open('./README.md').read(),
     long_description_content_type="text/markdown",
     install_requires=[
         "numpy>=1.15.3",
